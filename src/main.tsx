2ac580db659a0a5f906abc68ea4d6c2a8595c19c
Wilson Malafaia Peixoto <wilsonmp2@gmail.com>
2026-07-24 20:55:05 -0300

    fix: aplicar camada aprovada após estilos legados

--- src/main.tsx
+++ src/main.tsx
@@ -11,9 +11,9 @@ import "@fontsource/source-serif-4/latin-600.css";
 import "@fontsource/source-serif-4/latin-700.css";
 import App from "./App.tsx";
 import "./index.css";
-import "./styles/cover-intro-v5.css";
 import "./styles/institutional-polish.css";
 import "./styles/light-contrast.css";
 import "./styles/wcag-warning.css";
+import "./styles/cover-intro-v5.css";
 
 createRoot(document.getElementById("root")!).render(<App />);

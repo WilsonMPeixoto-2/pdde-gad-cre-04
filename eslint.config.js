da5b9fc78fb6a7f9e76a6997dc54933b30d383ae
Wilson Malafaia Peixoto <wilsonmp2@gmail.com>
2026-07-28 00:04:57 -0300

    chore: adicionar auditoria JSX de acessibilidade

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,40 @@
 import js from "@eslint/js";
 import globals from "globals";
+import jsxA11y from "@htmlacademy/eslint-plugin-jsx-a11y";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
+const jsxA11yRecommendedWarnings = Object.fromEntries(
+  Object.keys(jsxA11y.flatConfigs.recommended.rules).map((ruleName) => [ruleName, "warn"]),
+);
+
 export default tseslint.config(
-  { ignores: ["dist", "output"] },
+  { ignores: ["dist", "output", ".lighthouseci", "stats.html"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
     plugins: {
+      "jsx-a11y": jsxA11y,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
+    settings: {
+      "jsx-a11y": {
+        polymorphicPropName: "asChild",
+      },
+    },
     rules: {
+      ...jsxA11yRecommendedWarnings,
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
       "@typescript-eslint/no-unused-vars": [

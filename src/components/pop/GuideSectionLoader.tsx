0329a3365b28d03a18a07e0ef36196edd8baf59e
Wilson Malafaia Peixoto <wilsonmp2@gmail.com>
2026-07-28 01:15:49 -0300

    refactor: extrair skeleton das seções do guia

---
+++ src/components/pop/GuideSectionLoader.tsx
@@ -0,0 +1,8 @@
+export const GuideSectionLoader = () => (
+  <div className="min-h-[400px] space-y-4 p-6" aria-hidden="true">
+    <div className="h-6 w-3/4 rounded-lg skeleton-shimmer" />
+    <div className="h-4 w-full rounded-lg skeleton-shimmer" />
+    <div className="h-4 w-5/6 rounded-lg skeleton-shimmer" />
+    <div className="h-4 w-1/2 rounded-lg skeleton-shimmer" />
+  </div>
+);
